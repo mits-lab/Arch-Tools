010a66e362f45724495e830a28b723647be6cad0
MITS Labs <73417721+mits-lab@users.noreply.github.com>
2021-10-03 00:05:45 -0400

    added comments

--- batt/batt.sh
+++ batt/batt.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/bash
 
+# Monocle IT Solutions rev. 2021100203
+#
+# batt.sh
+#
+# Tested on Archlinux 2021.10.01 x86_64 (Kernel: 5.14.8)
+# Script should be run manually from the terminal. Outputs battery status and capacity on demand in a clean at-a-glance format.
+# Suggest adding 'batt' alias to .bashrc
+
 battnum=`cat /sys/class/power_supply/BAT0/capacity`
 battchargestate=`cat /sys/class/power_supply/BAT0/status`
 
