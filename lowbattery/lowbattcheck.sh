599c0052562aaf2f3ba738872f5758dd8d30e6af
MITS Labs <73417721+mits-lab@users.noreply.github.com>
2021-10-04 01:33:52 -0400

    added echo output to wall
    change permits broadcast notifications to all users.

--- lowbattery/lowbattcheck.sh
+++ lowbattery/lowbattcheck.sh
@@ -5,7 +5,7 @@
 # lowbattcheck.sh
 #
 # Tested on Archlinux 2021.10.01 x86_64 (Kernel: 5.14.8)
-# Script should be run from cron or a systemd timer on a 5 minute repeat. Notifies the terminal when your battery is running low.
+# Script should be run from root's cron or a systemd timer on a 5 minute repeat. Notifies the terminal when your battery is running low.
 
 battnum=`cat /sys/class/power_supply/BAT0/capacity`
 
@@ -15,7 +15,7 @@ normal='\e[0m'
 
 if [ ${battnum} > "15" ]
 then
-	echo -n -e "\n$red!!!$normal Connect Power Cable$red !!!$normal \n\n"
+	echo -n -e "\n$red!!!$normal Connect Power Cable$red !!!$normal \n\n" | wall -n
 	aplay --buffer-size=10 -q ~/Scripts/lowbattery.wav
 	exit 0
 else
